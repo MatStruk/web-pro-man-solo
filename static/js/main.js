65e3e18421dcd50d1480b4d9b47c907b45408255
Mateusz Struk <ms@mateuszstruk.pl>
2018-08-11 11:21:56 +0200

    Starting point for the project

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+// This function is to initialize the application
+function init() {
+    // init data
+    dataHandler.init();
+    // loads the boards to the screen
+    dom.loadBoards();
+
+}
+
+init();
